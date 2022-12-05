10b8ed2b250256261714106291932cdf445ecbb1
Nathan Crum <nathan.crum@outlook.com>
2022-12-05 03:30:50 +0000

    adds count by bytes and tests

--- github.com/crumdev/firstProgram/wc/main.go
+++ github.com/crumdev/firstProgram/wc/main.go
@@ -13,17 +13,23 @@ func main() {
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count lines")
+
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
 	// Define the scanner split type tp words (default is split by lines)
 	if !countLines {
-		scanner.Split(bufio.ScanWords)
+		if !countBytes {
+			scanner.Split(bufio.ScanWords)
+		} else {
+			scanner.Split(bufio.ScanBytes)
+		}
 	}
 
 	// defining a counter
--- github.com/crumdev/firstProgram/wc/main_test.go
+++ github.com/crumdev/firstProgram/wc/main_test.go
@@ -11,7 +11,8 @@ func TestCountWords(t *testing.T) {
 
 	exp := 4
 
-	res := count(b, false)
+	countLines, countBytes := false, false
+	res := count(b, countLines, countBytes)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead. \n", exp, res)
@@ -23,8 +24,22 @@ func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2\n word3 word4")
 
 	exp := 2
+	countLines, countBytes := true, false
+	res := count(b, countLines, countBytes)
 
-	res := count(b, true)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead. \n", exp, res)
+	}
+}
+
+// TestCountBytes tests the count function set to count lines
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word")
+
+	exp := 4
+
+	countLines, countBytes := false, true
+	res := count(b, countLines, countBytes)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead. \n", exp, res)
